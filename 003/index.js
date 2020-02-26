9c3647fb0a82c2d31b790c987f85b52b04e935d6
wangshuisheng <602466011@qq.com>
2020-02-26 16:10:49 +0800

    更新:添加token字段

--- 003/index.js
+++ 003/index.js
@@ -4,7 +4,7 @@ var app = express();
 //设置跨域访问
 app.all('*', function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With,token");
 	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
 	res.header("X-Powered-By", ' 3.2.1');
 	res.header("Content-Type", "application/json;charset=utf-8");
