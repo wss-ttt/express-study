802774542c98b03de0dbb32b76f38a3dfc703b72
wangshuisheng <602466011@qq.com>
2020-03-03 16:29:58 +0800

    更新:005中添加测试接口

--- 005/index.js
+++ 005/index.js
@@ -11,20 +11,6 @@ for (let i = 0; i < 500; i++) {
 		name: 'name' + i
 	})
 }
-/* var data = [
-	{
-		"id":1,
-		"name":"张三"
-	},
-	{
-		"id":2,
-		"name":"乔峰"
-	},
-	{
-		"id":3,
-		"name":"小龙女"
-	}
-]; */
 
 // 测试 接口
 app.get('/users/list', function (req, res) {
@@ -35,16 +21,113 @@ app.get('/users/list', function (req, res) {
 // 测试 
 app.get('/users/list2', function (req, res) {
 	res.json({
-		code:0,
-		msg:'success',
-		data:data
+		code: 0,
+		msg: 'success',
+		data: data
 	});
 });
 // 测试 post接口
-app.post('/users/delete',function(req,res){
+app.post('/users/delete', function (req, res) {
 	res.status(200);
 	res.json(data);
 });
+
+app.get('/echarts/test1/list', function (req, res) {
+	let xData = [];
+	for (let i = 0; i < 50; i++) {
+		xData.push('aaa' + i);
+	}
+	let yData = [];
+	for (let i = 0; i < 50; i++) {
+		let num = Math.floor(Math.random() * (1500 - 100 + 1) + 100) // 向下取整
+		yData.push(num)
+	}
+	let data = {
+		xAxis: xData,
+		yAxis: yData
+	}
+	res.json(data);
+})
+app.get('/echarts/test2/list', function (req, res) {
+	let xData = [];
+	for (let i = 0; i < 50; i++) {
+		xData.push('bbb' + i);
+	}
+	let yData = [];
+	for (let i = 0; i < 50; i++) {
+		let num = Math.floor(Math.random() * (1500 - 100 + 1) + 100) // 向下取整
+		yData.push(num)
+	}
+	let data = {
+		xAxis: xData,
+		yAxis: yData
+	}
+	res.json(data);
+})
+app.get('/echarts/test3/list', function (req, res) {
+	let xData = [];
+	for (let i = 0; i < 50; i++) {
+		xData.push('ccc' + i);
+	}
+	let yData = [];
+	for (let i = 0; i < 50; i++) {
+		let num = Math.floor(Math.random() * (1500 - 100 + 1) + 100) // 向下取整
+		yData.push(num)
+	}
+	let data = {
+		xAxis: xData,
+		yAxis: yData
+	}
+	res.json(data);
+})
+app.get('/echarts/test4/list', function (req, res) {
+	let xData = [];
+	for (let i = 0; i < 50; i++) {
+		xData.push('ddd' + i);
+	}
+	let yData = [];
+	for (let i = 0; i < 50; i++) {
+		let num = Math.floor(Math.random() * (1500 - 100 + 1) + 100) // 向下取整
+		yData.push(num)
+	}
+	let data = {
+		xAxis: xData,
+		yAxis: yData
+	}
+	res.json(data);
+})
+app.get('/echarts/test5/list', function (req, res) {
+	let xData = [];
+	for (let i = 0; i < 50; i++) {
+		xData.push('eee' + i);
+	}
+	let yData = [];
+	for (let i = 0; i < 50; i++) {
+		let num = Math.floor(Math.random() * (1500 - 100 + 1) + 100) // 向下取整
+		yData.push(num)
+	}
+	let data = {
+		xAxis: xData,
+		yAxis: yData
+	}
+	res.json(data);
+})
+app.get('/echarts/test6/list', function (req, res) {
+	let xData = [];
+	for (let i = 0; i < 50; i++) {
+		xData.push('fff' + i);
+	}
+	let yData = [];
+	for (let i = 0; i < 50; i++) {
+		let num = Math.floor(Math.random() * (1500 - 100 + 1) + 100) // 向下取整
+		yData.push(num)
+	}
+	let data = {
+		xAxis: xData,
+		yAxis: yData
+	}
+	res.json(data);
+})
 
 app.listen(3001, function () {
 	console.log('端口号3001 服务启动成功');
