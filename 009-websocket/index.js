11a418feaef874e3851056c739f1e846787659aa
王水胜 <602466011@qq.com>
2021-08-05 15:47:30 +0800

    更新: 注释信息

--- 009-websocket/index.js
+++ 009-websocket/index.js
@@ -1,6 +1,5 @@
 var ws = require('nodejs-websocket');
 var server = ws.createServer(function (socket) {
-  // 事件名称为text(读取字符串时，就叫做text)，读取客户端传来的字符串
   var count = 1;
   socket.on('text', function (str) {
     // 在控制台输出前端传来的消息　　
--- 009-websocket/index2.js
+++ 009-websocket/index2.js
@@ -1,12 +1,10 @@
 var ws = require('nodejs-websocket');
 var server = ws.createServer(function (socket) {
-  // 事件名称为text(读取字符串时，就叫做text)，读取客户端传来的字符串
 	var count = 1;
 	var timer = null
   socket.on('text', function (str) {
 		// 在控制台输出前端传来的消息　　
 		console.log(str);
-		// 定时向服务器发送消息
 		timer = setInterval(() => {
 			//向前端回复消息
 			socket.sendText('服务器端收到客户端端发来的消息了！' + count++);
