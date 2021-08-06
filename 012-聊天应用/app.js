efdf8f872d1f2f1761aa7fdb35ffc7becb78d7ea
王水胜 <602466011@qq.com>
2021-08-06 11:56:48 +0800

    更新: 添加首次和非首次判断

--- 012-聊天应用/app.js
+++ 012-聊天应用/app.js
@@ -1,25 +1,45 @@
-var ws = require("nodejs-websocket");
+const ws = require("nodejs-websocket");
 console.log("开始建立连接...")
 
-var [user1, user2, user1Ready, user2Ready] = [null, null, false, false];
+let [user1, user2, user1Ready, user2Ready] = [null, null, false, false];
 
 ws.createServer(function (conn) {
 	conn.on("text", function (str) {
-		console.log("收到的信息为:" + str)
-		console.log('类型', typeof str)
-		if (str === "user1") {
+		str = JSON.parse(str)
+		let {
+			name,
+			count,
+			msg
+		} = str
+		count = +count // 转换成数字类型数据
+		if (name === "user1") {
 			user1 = conn;
 			user1Ready = true;
 		}
-		if (str === "user2") {
+		if (name === "user2") {
 			user2 = conn;
 			user2Ready = true;
 		}
-		if (user2Ready) {
-			user2.sendText(str);
-		}
-		if (user1Ready) {
-			user1.sendText(str);
+		// 如果是首次发送的消息
+		if (!count) {
+			str = JSON.stringify(str)
+			console.log('首次;', '第', count, '次接受到的数据:', str)
+			return conn.sendText(str)
+		} else {
+			console.log('非首次;', '第' ,count, '次接受到的数据:', str)
+			// 如果不是首次
+			// 获取服务器当前系统时间
+			const myDate = new Date();
+			time = myDate.toLocaleTimeString()
+			str.time = time
+			str = JSON.stringify(str)
+
+			if (user1Ready) {
+				user1.sendText(str);
+			}
+			if (user2Ready) {
+				user2.sendText(str);
+			}
 		}
 	})
 	conn.on("close", function (code, reason) {
@@ -28,6 +48,6 @@ ws.createServer(function (conn) {
 	conn.on("error", function (code, reason) {
 		console.log("异常关闭")
 	});
-}).listen(8001, function() {
+}).listen(8001, function () {
 	console.log("WebSocket建立完毕")
 })
